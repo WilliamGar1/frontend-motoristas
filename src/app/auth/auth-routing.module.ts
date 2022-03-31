ba2fd4d809dc22a38c04e044f6664366b5f50ff7
William Garcia <williamjbg06@hotmail.com>
2022-03-30 21:43:45 -0600

    forgot y registro

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ForgotPassComponent } from './components/forgot-pass/forgot-pass.component';
 import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
 
 const routes: Routes = [
   {
     path: '',
     children: [
       {path: 'login', component: LoginComponent},
+      {path: 'forgot', component: ForgotPassComponent},
+      {path: 'register', component: RegisterComponent},
       {path: '**', redirectTo: 'login'}
     ]
   }
--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -3,19 +3,22 @@ import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { LoginComponent } from './components/login/login.component';
+import { ForgotPassComponent } from './components/forgot-pass/forgot-pass.component';
+import { RegisterComponent } from './components/register/register.component';
 
 
 @NgModule({
   declarations: [
     LoginComponent,
-
+    ForgotPassComponent,
+    RegisterComponent,
   ],
   imports: [
     CommonModule,
     AuthRoutingModule
   ],
   exports: [
-    LoginComponent
+    
   ]
 })
 export class AuthModule { }
