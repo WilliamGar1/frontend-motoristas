c7dd2214cb4a48a243ec493b815d5da10cc14db4
William Garcia <williamjbg06@hotmail.com>
2022-04-03 00:27:04 -0600

    Finalizado orden tomada

--- src/app/orders/components/order-taken/order-taken.component.ts
+++ src/app/orders/components/order-taken/order-taken.component.ts
@@ -8,6 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class OrderTakenComponent implements OnInit {
 
   taken: boolean = false;
+  hora = Date.now()
+  total: number = 250.56
 
   constructor() { }
 
