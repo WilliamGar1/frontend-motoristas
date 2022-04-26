0d19d291c993b09c9455b745b4d62afe4b62c42e
William Garcia <williamjbg06@hotmail.com>
2022-04-26 11:24:50 -0600

    login finalizado

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { Driver } from '../../models/driver-interface';
+import { SharedService } from '../../services/shared.service';
 
 @Component({
   selector: 'app-header',
@@ -8,24 +10,32 @@ import { Router } from '@angular/router';
 })
 export class HeaderComponent implements OnInit {
 
+  public driver: Driver;
+  public name;
+
   constructor(
-    private router: Router
+    private router: Router,
+    private shareService: SharedService
   ) { }
 
   ngOnInit(): void {
+    this.name = localStorage.getItem('name').split(' ')[0];
+  }
+
+  public getDriver(){
+
+    let id = localStorage.getItem('driver_id');
+    this.shareService.getDriver(id)
+      .subscribe(driver => {
+        this.driver = driver;
+      });
   }
 
   public out(){
     localStorage.removeItem('driver_id');
     localStorage.removeItem('token');
+    localStorage.removeItem('name');
     this.router.navigate(['home']);
-  }
-
-  get driverName(){
-
-    
-
-    return
   }
 
 }
